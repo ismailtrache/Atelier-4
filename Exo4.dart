37ca9c1310fcae25d9d61ff15db3cad4c314c1bc
ismailtrache <ismailtrache2@gmail.com>
2025-10-16 14:07:26 -0400

    Exercice 4 qestion 8

--- Exo4.dart
+++ Exo4.dart
@@ -17,3 +17,13 @@ class Livre extends Media {
     print("Ceci est un Livre : $titre par $auteur.");
   }
 }
+class Film extends Media {
+  final int dureeMinutes;
+
+  Film(String titre, this.dureeMinutes) : super(titre);
+
+  @override
+  void afficherType() {
+    print("Ceci est un Film : $titre (${dureeMinutes} min).");
+  }
+}
