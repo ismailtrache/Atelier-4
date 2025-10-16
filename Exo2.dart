802e49d6229daddd6631d5bccc28119c31e8e97d
ismailtrache <ismailtrache2@gmail.com>
2025-10-16 13:52:29 -0400

    Exercice 2 question 4

--- Exo2.dart
+++ Exo2.dart
@@ -3,4 +3,13 @@ class Point {
   double _y;
 
   Point(this._x, this._y);
+  
+  void move(double dx, double dy) {
+    _x += dx;
+    _y += dy;
+  }
+
+  void display() {
+    print("Point(x: $_x, y: $_y)");
+  }
 }
