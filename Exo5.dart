13694ecb28f66640ae4036bbbacb61cc78f85c51
ismailtrache <ismailtrache2@gmail.com>
2025-10-16 22:01:33 -0400

    Exercice 5 qestion 5

--- Exo5.dart
+++ Exo5.dart
@@ -5,3 +5,14 @@ abstract class Forme {
     print("Calcul de l'aire...");
   }
 }
+class Cercle extends Forme {
+  final double rayon;
+
+  Cercle(this.rayon);
+
+  @override
+  double calculerAire() {
+    return 3.1416 * rayon * rayon;
+  }
+}
+
