490d3ce1989234dde8a049d9aad68fba1412394e
ismailtrache <ismailtrache2@gmail.com>
2025-10-16 22:19:19 -0400

    Exercice 8  qestion 9

--- Exo8.dart
+++ Exo8.dart
@@ -19,12 +19,30 @@ class Livre {
   }
 }
 
+class Roman extends Livre {
+  String genre;
+
+  Roman(String titre, String auteur, this.genre) : super(titre, auteur);
+
+  void afficherRoman() {
+    print("Titre : $titre, Auteur : $auteur, Genre : $genre, Pages : $pages");
+  }
+}
+
 void main() {
   var livre1 = Livre("1984", "George Orwell");
   var livre2 = Livre("Le Petit Prince", "Antoine de Saint-Exupéry");
 
   livre1.afficherInfos();
   livre2.afficherInfos();
+
+  Livre.afficherTotalLivres();
+
+  var roman1 = Roman("Sherlock Holmes", "Arthur Conan Doyle", "Policier");
+  var roman2 = Roman("Les Misérables", "Victor Hugo", "Drame");
+
+  roman1.afficherRoman();
+  roman2.afficherRoman();
 
   Livre.afficherTotalLivres();
 }
